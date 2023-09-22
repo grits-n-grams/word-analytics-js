936cb81b4fbcd97701a6fcf54eccfb6108a119e9
gritsngrams <grits.n.grams@gmail.com>
2023-09-22 14:50:02 +1200

    refactor javascript

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const wordsNumberEl = document.querySelector(".stat__number--words");
 
 const inputHandler = () => {
   // validation
+
   if (textareaEl.value.includes("<script>")) {
     alert("You can't use <script>!");
     textareaEl.value = textareaEl.value.replace("<script>", "");
   }
-  //  update numbers
+
+  //  calculate numbers
+
   const numberOfCharacters = textareaEl.value.length;
   const twitterCharactersLeft = 280 - numberOfCharacters;
   const facebookCharactersLeft = 2200 - numberOfCharacters;
@@ -19,18 +22,18 @@ const inputHandler = () => {
   if (textareaEl.value.length === 0) {
     numberOfWords = 0;
   }
-  // visual indicator
-  if (twitterCharactersLeft < 0) {
-    twitterNumberEl.classList.add("stat__number--limit");
-  } else {
-    twitterNumberEl.classList.remove("stat__number--limit");
-  }
-  if (facebookCharactersLeft < 0) {
-    facebookNumberEl.classList.add("stat__number--limit");
-  } else {
-    twitterNumberEl.classList.remove("stat__number--limit");
-  }
+
+  // Visual indicator/ Change style to red when over word limit
+
+  twitterCharactersLeft < 0
+    ? twitterNumberEl.classList.add("stat__number--limit")
+    : twitterNumberEl.classList.remove("stat__number--limit");
+  facebookCharactersLeft < 0
+    ? facebookNumberEl.classList.add("stat__number--limit")
+    : facebookNumberEl.classList.remove("stat__number--limit");
+
   // set numbers
+
   wordsNumberEl.textContent = numberOfWords;
   charactersNumberEl.textContent = numberOfCharacters;
   twitterNumberEl.textContent = twitterCharactersLeft;
